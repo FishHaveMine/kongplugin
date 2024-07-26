e51696fb7d1d299fd2adf92f8f77b525441506e8
han <751174479@qq.com>
2024-07-26 14:39:23 +0800

    add

--- README.md
+++ README.md
@@ -1,18 +1,78 @@
 # kongplugin
 
-A new Flutter plugin project.
+美控热泵调试插件
 
 ## Getting Started
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
+依赖插件：
+  udp: 5.0.3        https://pub.dev/packages/udp
+  wifi_iot: ^0.3.19  https://pub.dev/packages/wifi_iot
 
-For help getting started with Flutter development, view the
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+注意使用Wi-Fi需要配置安卓的权限，iOS仅能进行Wi-Fi的扫描；
 
-The plugin project was generated without specifying the `--platforms` flag, no platforms are currently supported.
-To add platforms, run `flutter create -t plugin --platforms <platforms> .` in this directory.
-You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+## 使用流程：
+   如果手机已经连接了网关（美控设备）的AP：
+    第一步、toSearchGayway 进行广播扫描局域网内的设备；
+    第二步、toSearchGayway 回返回局域网内搜索到的设备IP列表及token，选择需要配置的IP、token 传入 initUDPservice 方法；
+    第三步、调用 getDeviceList、getSceneList、getScheduleList、getCommon 等获取配置文件json进行配置；
+    第四步、将配置过的数据 使用 sendDevicesConig 等下发到网关（美控设备）；
+
+### 引入流程：
+  1.在flutter项目的 pubspec.yaml 配置文件 dev_dependencies：下面添加
+
+  kongplugin:
+     git:
+      url: https://github.com/FishHaveMine/kongplugin.git
+
+  等待自动更新、或者使用 flutter pub get 进行更新；
+
+  2.页面上使用：
+
+  import 'package:flutter/material.dart';
+import 'package:flutter/src/animation/animation_controller.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/ticker_provider.dart';
+
+import 'package:kongplugin/kongplugin.dart';
+
+class kongtest extends StatefulWidget {
+  const kongtest({super.key});
+
+  @override
+  State<kongtest> createState() => _kongtestState();
+}
+
+class _kongtestState extends State<kongtest> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: () async {
+            List<dynamic> loadWifiList = await Kongplugin().loadWifiList();
+            //  获取Wi-Fi列表
+            print('loadWifiList:$loadWifiList');
+            for (var element in loadWifiList) {
+              print(element.ssid);
+            }
+            bool toCheckAP = await Kongplugin().toCheckAP();
+            print('toCheckAP:$toCheckAP');
+          },
+          child: Text("测试"),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/kongplugin.dart
+++ lib/kongplugin.dart
@@ -18,8 +18,28 @@ import 'package:wifi_iot/wifi_iot.dart';
 const String AP_DEFAULT_SSID = "HUAWEI_MK_ZT";
 const String AP_DEFAULT_PASSWORD = "Kong2023";
 
+Map<int, String> deviceType = {
+  1: 'devtype_xye_idu',
+  2: 'devtype_x1x2_idu',
+  3: 'devtype_wenkongqi_idu',
+  5: 'devtype_wkq_86m_idu',
+  6: 'devtype_x1x2ToRtu',
+  7: 'devtype_xye_v6_idu',
+  10: 'devtype_awm2611_sensor',
+  9: 'devtype_dds_sensor',
+  11: 'devtype_ksm_sensor',
+  12: 'devtype_mk3200',
+  13: 'devtype_mkb2',
+};
+Map<String, List> deviceTypeLimit = {
+  'deviceType-ac': [1, 2, 3, 5, 6, 7],
+  'deviceType-light': [12, 13],
+  'deviceType-sensor': [10, 11, 9]
+};
+
 class Kongplugin {
   UdpService? udpService;
+  int _sendConigError = 0;
 
   /// 获取Wi-Fi列表
   Future<List<WifiNetwork>> loadWifiList() async {
@@ -33,7 +53,7 @@ class Kongplugin {
     return htResultNetwork;
   }
 
-  toConnectAP() async {
+  Future<bool> toConnectAP() async {
     print("尝试连接WiFi");
 
     var _isConnected = await WiFiForIoTPlugin.isConnected();
@@ -43,6 +63,7 @@ class Kongplugin {
         return true;
       } else {
         bool isdisconnect = await WiFiForIoTPlugin.disconnect();
+        print("断开已有Wi-Fi，连接 悟空ap : $isdisconnect");
         if (!isdisconnect) {
           return false;
         }
@@ -155,17 +176,245 @@ class Kongplugin {
     } catch (e) {}
     return IPToken.isEmpty ? null : IPToken;
   }
+
+//设置udp服务的host和token
+  initUDPservice(host, token) {
+    udpService =
+        UdpService(address: InternetAddress(host), port: 8101, token: token);
+  }
+
+//获取硬件的设备列表
+  getDeviceList() async {
+    try {
+      var back = await udpService?.post({
+        "op": "get_config_deviceList",
+        "data": {},
+      });
+
+      List baseData = jsonDecode(jsonEncode(back['data']['deviceList']));
+      // idx=devtype*100000+a1*100+a2,
+      if (baseData.isNotEmpty) {
+        for (var element in baseData) {
+          if (element['idx'] != null) {
+            int idx = int.parse(element['idx'].toString());
+            element['t'] = idx ~/ 100000;
+
+            int a2 = int.parse(idx
+                .toString()
+                .substring(idx.toString().length - 1, idx.toString().length));
+            double a = ((idx - element['t'] * 100000) - a2) / 100;
+            element['a'] = a.toInt();
+            if (element['t'] == 12) element['a2'] = a2;
+
+            element['type'] = '--';
+            int tKey = int.parse(element['t'].toString());
+            if (deviceTypeLimit['deviceType-ac']!.contains(tKey)) {
+              element['type'] = 'AC';
+            }
+            if (deviceTypeLimit['deviceType-light']!.contains(tKey)) {
+              element['type'] = 'Light';
+            }
+            if (deviceTypeLimit['deviceType-sensor']!.contains(tKey)) {
+              element['type'] = 'Sensor';
+            }
+          }
+        }
+      }
+
+      const sortLimit = ['AC', 'Light', 'Sensor'];
+      baseData.sort((a, b) =>
+          sortLimit.indexOf(a['type']) - sortLimit.indexOf(b['type']));
+
+      return baseData;
+    } catch (e) {
+      return [];
+    }
+  }
+
+//获取硬件的场景列表
+  getSceneList() async {
+    try {
+      var back = await udpService?.post({
+        "op": "get_config_sceneList",
+        "data": {},
+      });
+      return back['data']['sceneList'];
+    } catch (e) {
+      return [];
+    }
+  }
+
+//获取硬件的日程列表
+  getScheduleList() async {
+    try {
+      var back = await udpService?.post({
+        "op": "get_config_scheduleList",
+        "data": {},
+      });
+
+      List baseData = jsonDecode(jsonEncode(back['data']['scheduleList']));
+      // BIT 0 1 2 3 4 5 6 7
+      //     0 6 5 7 3 2 1 7
+      List weekIN = [0, 6, 5, 4, 3, 2, 1, 7];
+      if (baseData.isNotEmpty) {
+        for (var element in baseData) {
+          if (element['weekdays'] != null) {
+            //                  0  6  5  4  3  2  1  7
+            List wdaysString = [0, 0, 0, 0, 0, 0, 0, 0];
+            for (int days = 1; days < weekIN.length; days++) {
+              if (element['weekdays'].contains(weekIN[days].toString())) {
+                wdaysString[days] = 1;
+              }
+            }
+
+            int wdays = int.parse(wdaysString.join(), radix: 2);
+            element['wdays'] = wdays;
+          } else if (element['weekdays'] == null && element['wdays'] != null) {
+            String binary =
+                int.parse(element['wdays'].toString()).toRadixString(2);
+            if (binary.length < 8) binary = binary.padLeft(8, '0');
+            List week = binary.split('');
+            element['weekdays'] = '';
+            for (int days = 1; days < weekIN.length; days++) {
+              if (week[days] == '1') {
+                element['weekdays'] =
+                    element['weekdays'] + weekIN[days].toString();
+              }
+            }
+          }
+        }
+      }
+      return jsonDecode(jsonEncode(baseData));
+    } catch (e) {
+      return [];
+    }
+  }
+
+//获取硬件的基本配置
+  getCommon() async {
+    try {
+      var back = await udpService?.post({
+        "op": "get_config_common",
+        "data": {},
+      });
+      return back['data']['common'];
+    } catch (e) {
+      return [];
+    }
+  }
+
+//更新硬件的时间
+  Future<bool> setDateTime() async {
+    try {
+      DateTime now = DateTime.now();
+
+      int year = now.year;
+      int month = now.month;
+      int day = now.day;
+      int weekday = now.weekday; // 1 = Monday, 7 = Sunday
+      int hour = now.hour;
+      int minute = now.minute;
+      int second = now.second;
+      var back = await udpService?.post({
+        "op": "setdatetime",
+        "data": {
+          'year': year,
+          'month': month,
+          'day': day,
+          'wday': weekday,
+          'hour': hour,
+          'minute': minute,
+          'second': second,
+        },
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+//下发devices数据到硬件
+  Future<bool> sendDevicesConig(devices) async {
+    try {
+      List device_List = [];
+      if (devices) {
+        for (var devices in devices) {
+          device_List.add({
+            "n": devices['n'],
+            "idx": devices['idx'],
+          });
+        }
+      }
+      var back = await udpService?.post({
+        "op": "set_config_deviceList",
+        "data": {"deviceList": device_List},
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+//下发 scenes 数据到硬件
+  Future<bool> sendScenesConig(scenes) async {
+    try {
+      var back = await udpService?.post({
+        "op": "set_config_sceneList",
+        "data": {"sceneList": scenes},
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+//下发 schedule 数据到硬件
+  Future<bool> sendScheduleConig(schedule) async {
+    try {
+      List sendScenes = [];
+      if (schedule) {
+        for (var schedule in schedule) {
+          sendScenes.add({
+            "n": schedule['n'],
+            "t": schedule['t'],
+            "wdays": schedule['wdays'],
+            "scIdx": schedule['scIdx']
+          });
+        }
+      }
+      var back = await udpService?.post({
+        "op": "set_config_scheduleList",
+        "data": {"scheduleList": sendScenes},
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+//下发 common 数据到硬件
+  Future<bool> sendCommonConig(common) async {
+    try {
+      var back = await udpService?.post({
+        "op": "set_config_common",
+        "data": {"common": common}
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 }
 
 int seq = 0;
-String token = '';
 String ipAddress = ''; // 搜索并链接的VRF设备IP
 
 class UdpService {
   final InternetAddress address;
   final int port;
+  final String token;
 
-  UdpService({required this.address, required this.port});
+  UdpService({required this.address, required this.port, required this.token});
 
   Future<dynamic> post(Map<String, dynamic> data) async {
     seq++;
